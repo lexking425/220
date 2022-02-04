b361480b5785b016d0f73d2f8b1c9ea68b0844c5
lexking425 <kinglf@g.cofc.edu>
2022-02-04 17:28:35 -0500

    Completed hw3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,54 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+Lex King
+hw3.py
+computing average, total tips, writing a sequence and outputting the approximation of pi.
+this is my code
 """
 
 
 def average():
-    pass
+    grades = eval(input("How many grades will you enter? "))
+    add = 0
+    total = 0
+    for i in range(grades):
+        enter_grades = eval(input("Enter Grade: "))
+        add = add + enter_grades
+    total = add / grades
+    print("average is:", total)
 
 
 def tip_jar():
-    pass
+    sum = 0
+    for i in range(5):
+        donate = eval(input("how much would you like to donate?"))
+        sum = sum + donate
+    print("total tips: ", sum)
 
 
 def newton():
-    pass
+    number = eval(input("What number do you want to square root?"))
+    approx = eval(input("How many times should we improve the approximation? "))
+    approx = (approx + number / approx) / 2
+    print("the square root is approximately", approx)
 
 
 def sequence():
-    pass
+    amount = eval(input("how many terms would you like? "))
+    terms = 1
+    for i in range(amount):
+        terms = terms + i % 2 * 2
+        print(terms, end='\t')
 
 
 def pi():
-    pass
+    terms = eval(input("Enter the number of terms in the series: "))
+    pi = 1
+    total = 1
+    for i in range(terms):
+        pi = pi + i % 2 * 2
+        y = (i + 1) / 2 + (i + 1)
+        total = total * (y / pi)
+    print(total * 2)
 
 
 if __name__ == '__main__':
